bd24434ad7babae64acb66b94df495194cda82c8
johnfelios <yiannisfelios@gmail.com>
2023-09-03 00:10:44 +0300

    update

--- code/server.js
+++ code/server.js
@@ -1,17 +1,11 @@
 const express = require('express');
 const mysql = require('mysql');
 const cors = require('cors');
-
-const app = express();
 const path = require('path');
 
-// Serve static files from the public directory
-app.use('/public', express.static(path.join(__dirname, 'public')));
+const app = express();
 
-// Serve map.html when its route is accessed
-app.get('/map.html', (req, res) => {
-    res.sendFile(path.join(__dirname, 'map.html'));
-});
+// Middleware
 app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
@@ -23,12 +17,24 @@ const connection = mysql.createConnection({
     database: 'web'
 });
 
-connection.connect();
+connection.connect((err) => {
+    if (err) throw err;
+    console.log('Connected to the database');
+});
 
- function validatePassword(password) {
+// Serve static files from the public directory
+app.use('/public', express.static(path.join(__dirname, 'public')));
+
+// Serve map.html when its route is accessed
+app.get('/map.html', (req, res) => {
+    res.sendFile(path.join(__dirname, 'map.html'));
+});
+
+function validatePassword(password) {
     const regex = /^(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+).{8,}$/;
     return regex.test(password);
- }
+}
+
 app.post('/api/add-user', (req, res) => {
     const { username, email, password } = req.body;
 
@@ -39,12 +45,38 @@ app.post('/api/add-user', (req, res) => {
     if (!validatePassword(password)) {
         return res.status(400).json({ error: 'Password must be at least 8 characters and contain an uppercase letter, a number, and a symbol.' });
     }
+
     const query = 'INSERT INTO user (username, email, password) VALUES (?, ?, ?)';
     connection.query(query, [username, email, password], (error, results) => {
         if (error) {
             return res.status(500).json({ error: 'Error inserting data' });
         }
         res.json({ success: true, message: 'User added successfully!' });
+    });
+});
+
+app.get('/getStores', (req, res) => {
+    const query = "SELECT * FROM store";
+    
+    connection.query(query, (err, results) => {
+        if (err) {
+            res.status(500).send('Server error');
+            throw err;
+        }
+        
+        const response = {
+            elements: results.map(store => ({
+                type: 'node',
+                lat: store.lat,
+                lon: store.lon,
+                tags: {
+                    name: store.name,
+                    shop: store.type
+                }
+            }))
+        };
+        
+        res.json(response);
     });
 });
 
